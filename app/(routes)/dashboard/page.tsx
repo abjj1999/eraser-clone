87aa7701a21c7cee047063badab9f6ca7971d847
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2024-04-07 22:30:26 -0500

    fixed convex query issue

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,25 +1,30 @@
 "use client";
 import React, { useEffect } from 'react'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
-import { useMutation, useQuery } from 'convex/react';
+import { useConvex, useMutation, useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 
 const Dashboard = () => {
-
+  const convex = useConvex();
   const {user}:any = useKindeBrowserClient();
 
-  const getUser = useQuery(api.user.getUser, {email: user?.email})
+  // const getUser = useQuery(api.user.getUser, {email: user?.email})
 
   const createUser = useMutation(api.user.createUser)
   useEffect(() => {
     if(user) {
-      if(getUser === undefined){
-        createUser({email: user.email, name: user.given_name, image: user.picture}).then((res) => {
-          console.log(res)
-        })
-      }
+      checkUser()
     }
   }, [user])
+
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser, {email: user?.email});
+    if(!result.length){
+      createUser({email: user.email, name: user.given_name, image: user.picture}).then((res) => {
+        console.log(res)
+      })
+    }
+  }
 
   return (
     <div>
